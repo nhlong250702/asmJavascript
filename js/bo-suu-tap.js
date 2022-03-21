2450bef53ba5a0f70c768e4616277668c511ecd8
Perry Nguyen <nhlong250702@gmail.com>
2022-03-21 13:46:13 +0700

    finish slide img

--- js/bo-suu-tap.js
+++ js/bo-suu-tap.js
@@ -1,8 +1,8 @@
 var arrSlide = [
     "0.JPG", "1.JPG", "2.JPG", "3.JPG", "4.JPG",
     "5.JPG", "6.jpg", "7.jpg", "8.jpg", "9.jpg",
-    "10jpg.", "11.jpg", "12.jpg", "13.jpg", "14.jpg",
-    "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg",
+    "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg",
+    "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg","20.jpg"
 ];
 
 var i = 0;
@@ -11,5 +11,11 @@ var vSlideImg = document.getElementById("mySlide");
 function fNext() {
     i++;
     if (i >= arrSlide.length) i = 0;
+    vSlideImg.src = "../images/" + arrSlide[i];
+}
+
+function fPrev() {
+    i--;
+    if (i < 0) i = arrSlide.length - 1;
     vSlideImg.src = "../images/" + arrSlide[i];
 }
