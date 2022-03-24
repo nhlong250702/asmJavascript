6cc22ac7e1e76a23e2d313aca22f6d176e861f17
Perry Nguyen <nhlong250702@gmail.com>
2022-03-24 11:01:06 +0700

    asm 1

--- js/bo-suu-tap.js
+++ js/bo-suu-tap.js
@@ -2,7 +2,7 @@ var arrSlide = [
     "0.JPG", "1.JPG", "2.JPG", "3.JPG", "4.JPG",
     "5.JPG", "6.jpg", "7.jpg", "8.jpg", "9.jpg",
     "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg",
-    "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg","20.jpg"
+    "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg"
 ];
 
 var i = 0;
@@ -13,13 +13,13 @@ function fNext() {
     i++;
     if (i >= arrSlide.length) i = 0;
     vSlideImg.src = "../images/" + arrSlide[i];
-    document.getElementById("indexSlide").innerHTML = "Ảnh " + i +" /20";
+    document.getElementById("indexSlide").innerHTML = "Ảnh " + (i+1) +" /20";
 }
 
 function fPrev() {
     i--;
     if (i < 0) i = arrSlide.length - 1;
     vSlideImg.src = "../images/" + arrSlide[i];
-    document.getElementById("indexSlide").innerHTML = "Ảnh " + i +" /20";
+    document.getElementById("indexSlide").innerHTML = "Ảnh " + (i+1) +" /20";
 }
 
