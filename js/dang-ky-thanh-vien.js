2323168bb0583c09939e0ebacc5f6bb0f328d8e6
Perry Nguyễn <nhlong250702@gmail.com>
2022-04-05 22:13:38 +0700

    asm 2 not finish signup member

---
+++ js/dang-ky-thanh-vien.js
@@ -0,0 +1,29 @@
+function validate() {
+    // var check = document.getElementsByClassName("khuon");
+    // // kiểm tra mã sinh viên, tên, email
+    // for (var i = 0; i < 3; i++) {
+    //     if (check[i].value == "") {
+    //         alert("bạn chưa nhập mã sinh viên/tên/email ?")
+    //     }
+    // }
+
+    // // kiểm tra giới tính
+    // var genders = document.getElementsByName('gioi-tinh');
+    // if (!genders[0].value.checked || !genders[1].value.checked) {
+    //     alert("chưa nhập giới tính")
+    // }
+
+    // //kiểm tra sở thích đã chọn chưa
+    // var hobbies = document.getElementsByName("hobbies");
+    // var count = 0;
+    // for (var i = 0; i < hobbies.length; i++) {
+    //     if(hobbies[i].checked) {
+    //         count ++;
+    //     }
+    // }
+    // if(count == 0) {
+    //     alert("chọn ít nhất một sở thích");
+    // }
+
+    
+}
