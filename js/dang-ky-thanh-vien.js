26d7e4ebdb62f137f46c9612f37e1743e334cba7
Perry Nguyễn <nhlong250702@gmail.com>
2022-04-07 09:26:54 +0700

    asm 2

--- js/dang-ky-thanh-vien.js
+++ js/dang-ky-thanh-vien.js
@@ -1,29 +1,96 @@
-function validate() {
-    // var check = document.getElementsByClassName("khuon");
-    // // kiểm tra mã sinh viên, tên, email
-    // for (var i = 0; i < 3; i++) {
-    //     if (check[i].value == "") {
-    //         alert("bạn chưa nhập mã sinh viên/tên/email ?")
-    //     }
-    // }
+var $ = document.querySelector.bind(document)
+var $$ = document.querySelectorAll.bind(document)
 
-    // // kiểm tra giới tính
-    // var genders = document.getElementsByName('gioi-tinh');
-    // if (!genders[0].value.checked || !genders[1].value.checked) {
-    //     alert("chưa nhập giới tính")
-    // }
+var form = $('#form')
+var mssv = $('#msv')
+var fullName = $('#name')
+var email = $('#email')
+var genders = $$('input[name="gender"]')
+var interests = $$('input[name="checkbox"]')
+var nationality = $('#nationality')
+var note = $('#note')
 
-    // //kiểm tra sở thích đã chọn chưa
-    // var hobbies = document.getElementsByName("hobbies");
-    // var count = 0;
-    // for (var i = 0; i < hobbies.length; i++) {
-    //     if(hobbies[i].checked) {
-    //         count ++;
-    //     }
-    // }
-    // if(count == 0) {
-    //     alert("chọn ít nhất một sở thích");
-    // }
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    validator()
+})
 
-    
+function validator() {
+    //Không để trống: mã, họ tên và email
+    if (mssv.value === '') {
+        errorMessage(mssv, 'Không để trống mã')
+    } else {
+        successMessage(mssv)
+    }
+
+    //Không để trống họ tên 
+    if (fullName.value === '') {
+        errorMessage(fullName, 'Không để trống họ tên ')
+    } else {
+        successMessage(fullName)
+    }
+
+    //Không để trống email 
+    if (email.value === '') {
+        errorMessage(email, 'Không để trống email ')
+    } else {
+        successMessage(email)
+    }
+
+    //Phải chọn giới tính
+    if (!genders[0].checked && !genders[1].checked) {
+        $('.gender').innerText = 'Phải chọn giới tính'
+        $('#frames').style.background = 'yellow'
+    } else {
+        $('.gender').innerText = ''
+        $('#frames').style.background = 'none'
+        $('#frames').style.borderColor = 'green'
+    }
+
+    //Phải chọn sở thích 
+    var check = 0
+    for (var i = 0; i < interests.length; i++) {
+        if (interests[i].checked == true) {
+            check++
+        }
+    }
+    if (check == 0) {
+        $('.hobby').innerText = 'Phải chọn sở thích'
+        $('#frames2').style.background = 'yellow'
+    } else {
+        $('.hobby').innerText = ''
+        $('#frames2').style.background = 'none'
+        $('#frames2').style.borderColor = 'green'
+    }
+
+    //Phải chọn quốc tịch
+    if (nationality.value == 0) {
+        $('.nationality').innerText = 'Phải chọn quốc tịch'
+        $('select').style.background = 'yellow'
+    } else {
+        $('.nationality').innerText = ''
+        $('select').style.background = 'none'
+        $('select').style.borderColor = 'green'
+    }
+
+    //Ít hơn 200 ký tự: ghi chú
+    if (note.value.length > 200) {
+        errorMessage(note, 'Ít hơn 200 ký tự')
+    } else {
+        successMessage(note)
+    }
+}
+
+//set lỗi
+function errorMessage(input, message) {
+    var formGroup = input.parentElement
+    var span = formGroup.querySelector('span')
+    formGroup.className = 'form-group error'
+    span.innerText = message
+}
+
+//set thành công
+function successMessage(input) {
+    var formGroup = input.parentElement
+    formGroup.className = 'form-group success'
 }
